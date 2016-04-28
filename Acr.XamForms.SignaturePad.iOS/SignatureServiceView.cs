46c1205a41269e8c3438b2fea3c83b2c6190fbbf
Leigh Shayler <leigh.shayler@gmail.com>
2016-04-28 09:10:57 +0800

    Updated to xamarin forms 2

--- Acr.XamForms.SignaturePad.iOS/SignatureServiceView.cs
+++ Acr.XamForms.SignaturePad.iOS/SignatureServiceView.cs
@@ -1,12 +1,11 @@
-using System;
 using CoreGraphics;
 using UIKit;
-using NativeView = SignaturePad.iOS.SignaturePadView;
+using NativeView = SignaturePad.SignaturePadView;
 
 
 namespace SignaturePad.iOS
 {
-	public class SignatureServiceView : UIView
+    public class SignatureServiceView : UIView
 	{
 		public NativeView Signature { get; set; }
 		public UIButton SaveButton { get; private set; }
